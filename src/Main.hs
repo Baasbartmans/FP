00a0940a6c68085d3a6a6876b309ed2889b07ff8
LAPTOP-9PR1HF00\bkolb <b.kolb@live.nl>
2017-11-02 17:13:28 +0100

    Merged

--- src/Main.hs
+++ src/Main.hs
@@ -3,9 +3,11 @@ module Main where
 import Controller
 import Model
 import View
+import Graphics.Gloss
+import Graphics.Gloss.Interface.Pure.Game
 
 import Graphics.Gloss.Interface.IO.Game
-
+{-
 main :: IO ()
 main = playIO (InWindow "Counter" (400, 400) (0, 0)) -- Or FullScreen
               black            -- Background color
@@ -14,3 +16,24 @@ main = playIO (InWindow "Counter" (400, 400) (0, 0)) -- Or FullScreen
               view             -- View function
               input            -- Event function
               step             -- Step function
+-}
+width, height, offset :: Int
+width = 300
+height = 300
+offset = 100
+              
+window :: Display
+window = InWindow "Pong" (width, height) (offset, offset)
+              
+background :: Color
+background = black
+              
+main :: IO ()
+main = do
+    -- load alle plaatjes
+    player <- loadBMP "D:/floor.bmp"
+                  
+    --teken de plaatjes
+    display window background (pictures [
+        drawing,
+        translate 30 50 $ player])
