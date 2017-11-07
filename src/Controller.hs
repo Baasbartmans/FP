7b212a0ff4b1446ab18f6ace2ee703baa097a82d
DESKTOP-QGL8FB8\dautz <s.p.dautzenberg@students.uu.nlcd>
2017-11-07 20:34:41 +0100

    Added button functionality - Added name to GameObject, so specific objects can be found

--- src/Controller.hs
+++ src/Controller.hs
@@ -3,10 +3,14 @@
 module Controller where
   
   import Model
-  
+  import Base
+  import GameObject
+  import Physics
   import Graphics.Gloss
   import Graphics.Gloss.Interface.IO.Game
   import System.Random
+  import Graphics.Gloss.Data.Point
+  import Scene
   
   -- | Handle one iteration of the game
   step :: Float -> GameState -> IO GameState
@@ -25,9 +29,23 @@ module Controller where
   input e gstate = return (inputKey e gstate)
   
   inputKey :: Event -> GameState -> GameState
+  inputKey (EventKey (MouseButton LeftButton) Down _ position) gstate@GameState{gsType=MainMenu} = 
+    -- if button levelSelect is pressed go to level selection gamestate, else stay in the MainMenu gameState
+    let object   = find "levelSelectBtn" (hud(scene gstate))
+    in if   clickedOn position object
+       then head $ gameStates gstate
+       else gstate
+
   {-inputKey (EventKey (Char c) _ _ _) gstate
     = undefined-} -- If the user presses a character key, show that one
   inputKey _ gstate = gstate -- Otherwise keep the same
+
+  clickedOn :: Position -> GameObject -> Bool
+  clickedOn pos obj = let box        = collisionBox $ rigidBody obj
+                          dimensions = size box
+                          p1@(x,y)   = position box
+                          p2         = (x + fromIntegral (fst dimensions), y + fromIntegral (snd dimensions))
+                      in  pointInBox p1 p2 pos
 
   update :: Float -> GameState -> IO GameState
   update elapsedTime gstate@GameState{gsType=MainMenu} 
