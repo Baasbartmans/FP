07eb09b09867affa69b84310b404cf20b6b2d99a
LAPTOP-9PR1HF00\bkolb <b.kolb@live.nl>
2017-11-11 16:47:15 +0100

    walking and jumping

--- src/Controller.hs
+++ src/Controller.hs
@@ -10,6 +10,7 @@ module Controller where
   import System.Random
   import Graphics.Gloss.Data.Point
   import Scene
+  import View
   
   -- | Handle one iteration of the game
   step :: Float -> GameStateManager -> IO GameStateManager
@@ -56,3 +57,47 @@ module Controller where
                           p1@(p,q)   = (x - fromIntegral (fst dimensions) * 0.5, y - fromIntegral (snd dimensions) * 0.5)
                           p2         = (p + fromIntegral (fst dimensions), q + fromIntegral (snd dimensions))
                       in  pointInBox pos p1 p2
+
+  movement :: Event -> GameObject -> GameObject
+  movement (EventKey (Char 'a') _ _ _) g            = walk g 0   -- walk left
+  movement (EventKey (Char 'd') _ _ _) g            = walk g 1   -- walk right
+  movement (EventKey (SpecialKey KeySpace) _ _ _) g = jump g 1 -- jump
+  movement _   g                                    = g        -- do nothing
+
+  jump :: GameObject -> Int -> GameObject
+  jump g@GameObject{rigidBody = r} i | i < 10 && i > 0 = jump g{rigidBody = rigid' r 2} (i + 1)
+                                     | i > 10          = jump g{rigidBody = rigid' r 3} (i - 1)
+                                     | otherwise       = g
+
+  walk :: GameObject -> Int -> GameObject
+  walk g@GameObject{rigidBody = r} 0 = g{rigidBody = rigid' r 0}
+  walk g@GameObject{rigidBody = r} 1 = g{rigidBody = rigid' r 1}   
+
+                                
+  rigid' :: RigidBody -> Int -> RigidBody
+  rigid' r@RigidBody{collisionBox = c} 0 = r{collisionBox = collisionHP c}
+  rigid' r@RigidBody{collisionBox = c} 1 = r{collisionBox = collisionHM c}
+  rigid' r@RigidBody{collisionBox = c} 2 = r{collisionBox = collisionJU c}
+  rigid' r@RigidBody{collisionBox = c} _ = r{collisionBox = collisionJD c}
+
+
+  --aanpastypes
+
+  collisionHP :: CollisionBox -> CollisionBox --Horizontal Plus
+  collisionHP c@CollisionBox{position = p} = c{position = (pos', snd p)}
+                                      where pos' = 1 + (fst p)
+
+  collisionHM :: CollisionBox -> CollisionBox --Horizontal Min
+  collisionHM c@CollisionBox{position = p} = c{position = (pos', snd p)}
+                                      where pos' = (-1) + (fst p)
+
+  collisionJU :: CollisionBox -> CollisionBox --Jump Up
+  collisionJU c@CollisionBox{position = p} = c{position = (fst p, pos')}
+                                      where pos' = 1 + (snd p)
+
+  collisionJD :: CollisionBox -> CollisionBox --Jump down
+  collisionJD c@CollisionBox{position = p} = c{position = (fst p, pos')}
+                                      where pos' = (-1) + (fst p)
+
+
+                                        
